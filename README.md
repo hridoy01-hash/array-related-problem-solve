95d333ec142f0d49e4442fdbef007e01952a17fe
hridoye <hridoysagor171@gmail.comm>
2022-09-13 23:10:48 -0600

    array basic

---
+++ README.md
@@ -0,0 +1,1 @@
+# array-related-problem-solve
---
+++ arrray.js
@@ -0,0 +1,11 @@
+const UserName = ["sagor", "tokir", "sahin", "raju", "abir"];
+
+// push
+UserName.push(120);
+console.log("afer 120 push", UserName);
+
+// pop
+UserName.pop();
+console.log("after pop", UserName);
+
+// delete
